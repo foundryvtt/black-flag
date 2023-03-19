23064ce04c9bb69eb310ef3202f9beff1f68811b
akrigline <a.krigline@gmail.com>
2023-03-19 15:46:01 -0400

    feat: add target data model

--- system/CONSTANTS.mjs
+++ system/CONSTANTS.mjs
@@ -9,6 +9,20 @@ function log(...args) {
   if ( isDebugging() ) console.log(`${SYSTEM_NAME} |`, ...args);
 }
 
+const DISTANCE_UNITS = {
+  ft: {label: "ft"},
+  mi: {label: "mi"}
+};
+
+const AREA_OF_EFFECT_TYPES = {
+  sphere: {label: "sphere"},
+  cylinder: {label: "cylinder"},
+  cone: {label: "cone"},
+  square: {label: "square"},
+  cube: {label: "cube"},
+  line: {label: "line"}
+};
+
 /**
  * The supported skill types.
  * @type {{label: string}}
@@ -173,8 +187,8 @@ const TALENT_CATEGORIES = {
  */
 const HIT_DIE_TYPES = {
   d6: {label: "d6"},
-  d10: {label: "d10"},
-}
+  d10: {label: "d10"}
+};
 
 /**
  * The supported ability types.
@@ -186,7 +200,7 @@ const ABILITY_TYPES = {
   intelligence: {label: "Intelligence", shorthand: "INT"},
   wisdom: {label: "Wisdom", shorthand: "WIS"},
   charisma: {label: "Charisma", shorthand: "CHA"}
-}
+};
 
 /**
  * Given a level, return the XP required to the level.
@@ -243,6 +257,8 @@ export const SYSTEM = {
   RACE_SIZE_TYPES,
   ALIGNMENT_TYPES,
   EQUIPMENT_SIZES,
+  DISTANCE_UNITS,
+  AREA_OF_EFFECT_TYPES,
   TALENT_CATEGORIES,
   BACKGROUND_DOCUMENTS: new Collection(),
   HERITAGE_DOCUMENTS: new Collection(),
---
+++ system/datamodels/item/common/target.mjs
@@ -0,0 +1,80 @@
+/**
+ * Data model template for a Target, as may be used by Powers
+ *
+ * @property {string} type
+ *
+ * @property {object} range
+ * @property {string} range.type
+ * @property {string} range.unit
+ * @property {number} range.value
+ *
+ * @property {object} areaOfEffect
+ * @property {number} areaOfEffect.primaryDimension
+ * @property {number} areaOfEffect.secondaryDimension
+ * @property {string} areaOfEffect.type
+ * @property {string} areaOfEffect.unit
+ *
+ * @property {object} targets
+ * @property {boolean} areaOfEffect.affectsObjects
+ * @property {string} areaOfEffect.creatures
+ * @property {number} areaOfEffect.numberOfChoices
+ *
+ * @mixin
+ */
+export default class TargetDataModel extends foundry.abstract.DataModel {
+
+  /** @inheritdoc */
+  static _enableV10Validation = true;
+
+  /** @inheritDoc */
+  static defineSchema() {
+    const fields = foundry.data.fields;
+    return {
+      // Required if type === 'aoe'
+      areaOfEffect: new fields.SchemaField({
+        primaryDimension: new fields.NumberField(),
+        secondaryDimension: new fields.NumberField(),
+        type: new fields.StringField({
+          choices: CONFIG.SYSTEM.AREA_OF_EFFECT_TYPES
+        }),
+        unit: new fields.StringField({
+          choices: CONFIG.SYSTEM.DISTANCE_UNITS,
+          initial: "ft"
+        })
+      }),
+
+      // Required if type !== 'self'
+      range: new fields.SchemaField({
+        type: new fields.StringField({
+          initial: "distance",
+          choices: ["distance" | "touch" | "unlimited"]
+        }),
+        // Required if type === 'distance'
+        unit: new fields.StringField({
+          choices: CONFIG.SYSTEM.DISTANCE_UNITS,
+          initial: "ft"
+        }),
+        // Required if type === 'distance'
+        value: new fields.NumberField()
+      }),
+
+      // Required if type !== 'self'
+      targets: new fields.SchemaField({
+        affectsObjects: new fields.BooleanField({
+          required: true,
+          initial: false
+        }),
+        creatures: new fields.StringField({
+          choices: ["all", "allies", "enemies", "choice", "none"]
+        }),
+        // Required if creatures === 'choice'
+        numberOfChoices: new fields.NumberField()
+      }),
+
+      type: new fields.StringField({
+        required: true,
+        choices: ["self" | "within-range" | "aoe"]
+      })
+    };
+  }
+}
